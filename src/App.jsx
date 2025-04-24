377e2536c038d22775f330fb44ff48cfcee9043b
harpreet kaur gothra <harpreetk2146@gmail.com>
2025-04-24 23:58:14 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import "./styles.css"
+
+export default function App(){
+  return (
+  <>
+  <form className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="task" id="item"></label>
+      <input type="text" id="item"/>
+    </div>
+    <button className="btn">Add</button>
+  </form>
+  <h1 className="header">Todo List</h1>
+  <ul className="list">
+    <li>
+      <label>
+        <input type="checkbox"/>
+        item 1
+      </label>
+      <button className="btn btn-danger">Delete</button>
+    </li>
+    <li>
+      <label>
+        <input type="checkbox"/>
+        item 2
+      </label>
+      <button className="btn btn-danger">Delete</button>
+    </li>
+  </ul>
+
+  </>
+)}
